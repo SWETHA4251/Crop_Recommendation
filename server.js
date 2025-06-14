d53dfd93f2c85499f60dc726565ba12b285f7a05
SWETHA4251 <153160953+SWETHA4251@users.noreply.github.com>
2025-06-14 17:36:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -91,4 +91,4 @@ app.post('/predict', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('🚀 Server is running on port ${PORT}'));
+app.listen(PORT, () => console.log(`🚀 Server is running on port ${PORT}`));
